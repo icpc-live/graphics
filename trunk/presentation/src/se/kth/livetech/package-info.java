c9f29915bb2fe438735ad0132f3c4de23b8c9562
hammond <none@none>
2009-09-13 19:43:30 +0000

    Added livetech package-info.
    --HG--
    extra : convert_revision : svn%3A5bd50d1f-0cf7-417a-b3c6-48be4479ebb3%4016

---
+++ trunk/presentation/src/se/kth/livetech/package-info.java
@@ -0,0 +1,17 @@
+package se.kth.livetech;
+
+
+/**
+ * 
+ * Contest model: new + inspired by kth.contest
+ * 
+ * Properties: latest version from ICPC Live 2009 + ui classes
+ * 
+ * Thift + class loading: latest version from kth.communication
+ * 
+ * Layout + animation: inspired by ICPC Live 2009 classes
+ * 
+ * 3D graphics: kth.scene + surveillance
+ * 
+ * 
+ */

fb2bd597d4544351dfea29d71f05541f71619ccc
marcus <none@none>
2009-11-17 20:44:36 +0000

    Rewrote the thrift protocol specification.
    --HG--
    extra : convert_revision : svn%3A5bd50d1f-0cf7-417a-b3c6-48be4479ebb3%4031

--- trunk/presentation/src/se/kth/livetech/communication/communication.thrift
+++ trunk/presentation/src/se/kth/livetech/communication/communication.thrift
@@ -4,64 +4,50 @@
  * Scoreboard Communication thrift interface declarations.
  */
 
-namespace java kth.communication
-//TODO: change namespace to java se.kth.livetech.communication
+namespace java se.kth.livetech.communication
 
 struct Node {
-  1: string kind,
-  2: string addr,
-  3: i32 port
+  1: string name,
+  2: string address,
+  3: i32 port,
 }
 
-struct Attr {
-  1: string key,
-  2: string value
-}
-
-struct Attrs {
+struct ContestEvent {
   1: i64 time,
   2: string type,
-  3: list<Attr> properties
+  3: map<string, string> properties,
 }
 
 
 service Service {
-  // Time
+	// Node registration
+	void attach(1:Node node),
+	void detach(),
+	
+  // Server time
   i64 time(),
-  i64 ping(1:i64 clock),
-  async void asyncPing(1:Node node, 2:i64 clock, 3:i64 ping),
 
   // Code
+  binary getClass(1:string className),
   async void classUpdate(1:string className),
-  binary getClass(1:string className)
-}
-
-
-/** Spider man */
-service SpiderService extends Service {
+  
+  // Resources
+  binary getResource(1:string resourceName),
+  async void resourceUpdate(1:string resourceName),
+  
   // Nodes
   void addNode(1:Node node),
   void removeNode(1:Node node),
   list<Node> getNodes(),
-  list<Node> getNodesOfKind(1:string kind),
 
-  #// Ports
-  #i32 getFreePort(1:string kind, 2:string addr),
+  // Properties
+  map<string, string> getParameters(),
+  string getProperty(1:string key),
+  void setProperty(1:string key, 2:string value),
 
-  #// Contest
-  #list<Attrs> getAttrs(),
-  #list<Attrs> getAttrsSince(1:i64 since),
-  async void contestUpdate(1:Attrs attrs),
-  // Parameters
-  map<string, string> getParameters(1:Node node),
-  string getParameter(1:Node node, 2:string key),
-  void setParameter(1:Node node, 2:string key, 3:string value)
-}
-
-
-service UpdateService extends Service {
-  async void parameterUpdate(2:string key, 3:string value),
-  async void contestUpdate(1:Attrs attrs),
+  // Updates
+  async void propertyUpdate(1:string key, 2:string value),
+  async void contestUpdate(1:ContestEvent event),
 }
 
 

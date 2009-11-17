d5558b444ca48778c80ff23f390b98cec0ea7f4d
hammond <none@none>
2009-11-17 20:52:19 +0000

    Soft hash map
    --HG--
    extra : convert_revision : svn%3A5bd50d1f-0cf7-417a-b3c6-48be4479ebb3%4032

--- trunk/presentation/src/se/kth/livetech/util/SoftHashMap.java
+++ trunk/presentation/src/se/kth/livetech/util/SoftHashMap.java
@@ -1,31 +1,96 @@
 package se.kth.livetech.util;
 
-import java.util.AbstractMap;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.lang.ref.*;
+import java.util.*;
+import java.io.Serializable;
 
-public class SoftHashMap<K,V> extends AbstractMap<K,V> {
-	private final Map<K,V> hash = new HashMap<K,V>();
+public class SoftHashMap <K, V> extends AbstractMap<K, V>
+    implements Serializable {
+    /** The internal HashMap that will hold the SoftReference. */
+    private final Map<K, SoftReference<V>> hash = new HashMap<K, SoftReference<V>>();
+    private final Map<SoftReference<V>, K> reverseLookup = new HashMap<SoftReference<V>, K>();
+
+    /** Reference queue for cleared SoftReference objects. */
+    private final ReferenceQueue<V> queue = new ReferenceQueue<V>();
 
     public V get(Object key) {
-    	return hash.get(key);
+	expungeStaleEntries();
+	V result = null;
+	// We get the SoftReference represented by that key
+	SoftReference<V> soft_ref = hash.get(key);
+	if (soft_ref != null) {
+	    // From the SoftReference we get the value, which can be
+	    // null if it has been garbage collected
+	    result = soft_ref.get();
+	    if (result == null) {
+		// If the value has been garbage collected, remove the
+		// entry from the HashMap.
+		hash.remove(key);
+		reverseLookup.remove(soft_ref);
+	    }
+	}
+	return result;
     }
 
-	public V put(K key, V value) {
-		return hash.put(key, value);
+    private void expungeStaleEntries() {
+	Reference<? extends V> sv;
+	while ((sv = queue.poll()) != null) {
+	    hash.remove(reverseLookup.remove(sv));
 	}
-	public void remove(K key) {
-		hash.remove(key);
+    }
+
+    public V put(K key, V value) {
+	expungeStaleEntries();
+	SoftReference<V> soft_ref = new SoftReference<V>(value, queue);
+	reverseLookup.put(soft_ref, key);
+	SoftReference<V> result = hash.put(key, soft_ref);
+	if (result == null) return null;
+	reverseLookup.remove(result);
+	return result.get();
+    }
+
+    public V remove(Object key) {
+	expungeStaleEntries();
+	SoftReference<V> result = hash.remove(key);
+	if (result == null) return null;
+	return result.get();
+    }
+
+    public void clear() {
+	hash.clear();
+	reverseLookup.clear();
+    }
+
+    public int size() {
+	expungeStaleEntries();
+	return hash.size();
+    }
+
+    /**
+     * Returns a copy of the key/values in the map at the point of
+     * calling.  However, setValue still sets the value in the
+     * actual SoftHashMap.
+     */
+    public Set<Entry<K,V>> entrySet() {
+	expungeStaleEntries();
+	Set<Entry<K,V>> result = new LinkedHashSet<Entry<K, V>>();
+	for (final Entry<K, SoftReference<V>> entry : hash.entrySet()) {
+	    final V value = entry.getValue().get();
+	    if (value != null) {
+		result.add(new Entry<K, V>() {
+			public K getKey() {
+			    return entry.getKey();
+			}
+			public V getValue() {
+			    return value;
+			}
+			public V setValue(V v) {
+			    entry.setValue(new SoftReference<V>(v, queue));
+			    return value;
+			}
+		    });
+	    }
 	}
-	public void clear() {
-		hash.clear();
-	}
-	public int size() {
-		return hash.size();
-	}
-	public Set<Map.Entry<K, V>> entrySet() {
-		// TODO: why:? throw new UnsupportedOperationException();
-		return hash.entrySet();
-	}	
+	return result;
+    }
 }

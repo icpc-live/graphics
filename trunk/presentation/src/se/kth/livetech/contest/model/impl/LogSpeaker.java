b452ba3c1dff10e2ed0e8761c2830cab55a46320
hakante <none@none>
2009-11-18 00:31:25 +0000

    Using SAXParser in LogSpeaker
    --HG--
    extra : convert_revision : svn%3A5bd50d1f-0cf7-417a-b3c6-48be4479ebb3%4033

--- trunk/presentation/src/se/kth/livetech/contest/model/impl/LogSpeaker.java
+++ trunk/presentation/src/se/kth/livetech/contest/model/impl/LogSpeaker.java
@@ -1,25 +1,31 @@
 package se.kth.livetech.contest.model.impl;
 
-import java.io.BufferedReader;
+import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.InputStream;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
 import se.kth.livetech.contest.model.AttrsUpdateEvent;
 import se.kth.livetech.contest.model.AttrsUpdateListener;
 
 /** Reads a log file, report it to {@link AttrsUpdateListener}s. */
 public class LogSpeaker {
-	Reader reader;
+	InputStream stream;
 	List<AttrsUpdateListener> listeners;
 
 	public LogSpeaker(String logName) throws FileNotFoundException {
-		reader = new InputStreamReader(new FileInputStream(logName));
-		reader = new BufferedReader(reader);
+		stream = new BufferedInputStream(new FileInputStream(logName));
 		listeners = new CopyOnWriteArrayList<AttrsUpdateListener>();
 	}
 
@@ -36,97 +42,75 @@ public class LogSpeaker {
 			listener.attrsUpdated(e);
 	}
 
-	private int tok;
-
-	private void lex() throws IOException {
-		tok = reader.read();
-	}
-
-	private String readString(boolean tag) throws IOException {
-		StringBuffer s = new StringBuffer();
-		while (tok >= 0 && (tag ? tok != '/' && tok != '>' : tok != '<')) {
-			s.append((char) tok);
-			lex();
-		}
-		return s.toString();
-	}
-
-	private static String format(String x) {
-		x = x.replace("&amp;", "&");
-		x = x.replace("&lt;", "<");
-		x = x.replace("&gt;", ">");
-		x = x.replace("&quot;", "\"");
-		return x;
-	}
-
 	public void parse() throws IOException {
-		AttrsUpdateEventImpl attrs = null;
-		lex();
-		int level = 0;
-		String name = null;
-		while (tok >= 0) {
-			if (tok == '>')
-				lex();
-			if (tok != '<') {
-				String value = readString(false);
-				if (level == 3)
-					attrs.setProperty(name, format(value));
-			} else {
-				lex();
-				if (tok != '/') {
-					String tag = readString(true);
+		try {
+			SAXParser sp = SAXParserFactory.newInstance().newSAXParser();
+			sp.parse(stream, new DefaultHandler() {
+
+				private int level = 0;
+				private String name = null;
+				private String value = null;
+				private AttrsUpdateEventImpl attrs = null;
+
+				@Override
+				public void characters(char[] ch, int start, int length)
+						throws SAXException {
+					value = new String(ch, start, length);
+				}
+
+				@Override
+				public void startElement(String uri, String localName,
+						String qName, Attributes attributes)
+						throws SAXException {
 					switch (++level) {
 					case 1:
-						if (!tag.equals("contest"))
-							new Error(tag + " != contest").printStackTrace();
+						if (!qName.equals("contest"))
+							new Error(qName + " != contest").printStackTrace();
 						break;
 					case 2:
-						String time = "0";
-						int i2 = tag.lastIndexOf('"');
-						if (i2 > 0) {
-							int i1 = tag.lastIndexOf('"', i2 - 1);
-							if (i1 > 0) {
-								// System.err.println("" + i1 + "/" + i2 + "/" +
-								// tag.length());
-								time = tag.substring(i1 + 1, i2);
-								int i3 = tag.indexOf(' ');
-								tag = tag.substring(0, i3 < 0 ? i1 : i3);
-							}
-						}
-						attrs = new AttrsUpdateEventImpl(Long.valueOf(time),
-								tag);
+						// TODO: Check what the old code actually did here.
+						// Might we get the time somehow?
+						attrs = new AttrsUpdateEventImpl(0, qName);
 						break;
 					case 3:
-						name = tag;
+						name = qName;
+						value = null;
 						break;
 					default:
-						new Error("xml too deep").printStackTrace();
+						new Error("invalid xml depth " + level).printStackTrace();
 					}
-				} else {
-					lex();
-					String tag = readString(true);
+				}
+
+				@Override
+				public void endElement(String uri, String localName,
+						String qName) throws SAXException {
 					switch (level--) {
 					case 1:
-						if (!tag.equals("contest"))
-							new Error(tag + " != contest").printStackTrace();
+						if (!qName.equals("contest"))
+							new Error(qName + " != contest").printStackTrace();
 						break;
 					case 2:
-						if (!tag.equals(attrs.getType()))
-							new Error(tag + " != " + attrs.getType())
-									.printStackTrace();
+						if (!qName.equals(attrs.getType()))
+							new Error(qName + " != " + attrs.getType()).printStackTrace();
 						send(attrs);
 						attrs = null;
 						break;
 					case 3:
-						if (!tag.equals(name))
-							new Error(tag + " != " + name).printStackTrace();
+						if (!name.equals(qName))
+							new Error(name + " != " + qName).printStackTrace();
+						attrs.setProperty(name, value);
 						name = null;
 						break;
 					default:
-						new Error("xml shallow").printStackTrace();
+						new Error("invalid xml depth " + (level+1)).printStackTrace();
 					}
 				}
-			}
+
+			});
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
 		}
 	}
 }

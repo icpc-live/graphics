1aa38a5b365809c5da95731039bbda09a223e624
zalenski <none@none>
2009-11-22 15:55:53 +0000

    #9 Updated thrift communications protocol.
    --HG--
    extra : convert_revision : svn%3A5bd50d1f-0cf7-417a-b3c6-48be4479ebb3%4036

--- trunk/presentation/src/se/kth/livetech/communication/communication.thrift
+++ trunk/presentation/src/se/kth/livetech/communication/communication.thrift
@@ -1,57 +1,87 @@
 #!/usr/local/bin/thrift --gen java:beans --gen java --gen py
 
 /**
- * Scoreboard Communication thrift interface declarations.
+ * ICPC Live Communication thrift interface declarations.
  */
 
-namespace java se.kth.livetech.communication
+namespace java se.kth.livetech.communication.thrift
 
 struct Node {
-  1: string name,
-  2: string address,
-  3: i32 port,
+	1: string name,
+	2: string address,
+	3: i32 port,
+}
+
+struct NodeStatus {
+	1: string name,
+	2: i64 lastContact,
+	3: i32 ping,
 }
 
 struct ContestEvent {
-  1: i64 time,
-  2: string type,
-  3: map<string, string> properties,
+	1: i64 time,
+	2: string type,
+	3: map<string, string> attributes,
 }
 
+struct PropertyEvent {
+	1: string key,
+	2: string value,
+}
 
-service Service {
+enum LogLevel {
+	spam, debug, info, notice, warning, error, critical,
+}
+
+struct LogEvent {
+	1: i64 time,
+	2: LogLevel level,
+	3: string message,
+	4: optional string origin,
+	5: optional string file,
+	6: optional i32 line,
+}
+
+exception LiveException {
+	1: string message,
+}
+
+service LiveService {
 	// Node registration
 	void attach(1:Node node),
 	void detach(),
-	
-  // Server time
-  i64 time(),
 
-  // Code
-  binary getClass(1:string className),
-  async void classUpdate(1:string className),
-  
-  // Resources
-  binary getResource(1:string resourceName),
-  async void resourceUpdate(1:string resourceName),
-  
-  // Nodes
-  void addNode(1:Node node),
-  void removeNode(1:Node node),
-  list<Node> getNodes(),
+	// Nodes
+	void addNode(1:Node node),
+	void removeNode(1:Node node),
+	list<Node> getNodes(),
+	list<NodeStatus> getNodeStatus(),
 
-  // Properties
-  map<string, string> getParameters(),
-  string getProperty(1:string key),
-  void setProperty(1:string key, 2:string value),
+	// Server time
+	i64 time(),
 
-  // Updates
-  async void propertyUpdate(1:string key, 2:string value),
-  async void contestUpdate(1:ContestEvent event),
+	// Code
+	binary getClass(1:string className),
+	oneway void classUpdate(1:string className),
+
+	// Resources
+	binary getResource(1:string resourceName),
+	oneway void resourceUpdate(1:string resourceName),
+
+	// Properties
+	map<string, string> getProperties(),
+	string getProperty(1:string key),
+	void setProperty(1:string key, 2:string value),
+
+	// Updates
+	oneway void propertyUpdate(1:PropertyEvent event),
+	oneway void contestUpdate(1:ContestEvent event),
+
+	// Logging
+	oneway void logEvent(1:LogEvent event),
+	oneway void logSubscribe(1:LogEvent template),
+	oneway void logUnsubscribe(1:LogEvent template),
 }
 
 
-#exception Err {
-#  1: string message
-#}
 #void err() throws (1:Err err)

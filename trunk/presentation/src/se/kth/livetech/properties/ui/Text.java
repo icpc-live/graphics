43a94f9ea17e7e0bc550da5d67f26c7446b0d79c
Mikael Auno <auno@kth.se>
2010-02-04 22:15:26 +0800

    Added highlighting of changed but unsaved text fields.

--- trunk/presentation/src/se/kth/livetech/properties/ui/Text.java
+++ trunk/presentation/src/se/kth/livetech/properties/ui/Text.java
@@ -1,7 +1,10 @@
 package se.kth.livetech.properties.ui;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JTextField;
 
@@ -9,14 +12,17 @@ import se.kth.livetech.properties.IProperty;
 import se.kth.livetech.properties.PropertyListener;
 
 @SuppressWarnings("serial")
-public class Text extends JTextField implements ActionListener, PropertyListener {
-	IProperty property;
-	String name;
+public class Text extends JTextField implements ActionListener, PropertyListener, KeyListener {
+	private IProperty property;
+	private String name;
+	private Color defaultBackground;
 	public Text(IProperty property) {
 	    //this.setFocusTraversalKeysEnabled(false);
 		this.property = property;
+		this.defaultBackground = this.getBackground();
 		property.addPropertyListener(this);
 		this.addActionListener(this);
+		this.addKeyListener(this);
 	}
 	public void actionPerformed(ActionEvent e) {
 		//DebugTrace.trace("text action " + this.getText());
@@ -27,5 +33,25 @@ public class Text extends JTextField implements ActionListener, PropertyListener
 		String value = this.property.getValue();
 		this.setText(value == null ? "" : value);
 	}
+	@Override
+	public void keyPressed(KeyEvent e) {}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		switch (e.getKeyChar()) {
+			case KeyEvent.VK_ESCAPE:
+				this.setText(this.property.getValue());
+			case KeyEvent.VK_ENTER:
+				this.setBackground(this.defaultBackground);
+				break;
+			default:
+				if (this.getText().equals(this.property.getValue())) {
+					this.setBackground(this.defaultBackground);
+				} else {
+					this.setBackground(Color.yellow);
+				}
+		}
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {}
 }
 

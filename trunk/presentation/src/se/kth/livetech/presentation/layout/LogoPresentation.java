90867c7de29aebe4523a52b4a5f519ec5a5e60cf
Elias Freider <elias@freider.se>
2010-02-05 08:12:17 +0800

    #13 bigger countdown

--- trunk/presentation/src/se/kth/livetech/presentation/layout/LogoPresentation.java
+++ trunk/presentation/src/se/kth/livetech/presentation/layout/LogoPresentation.java
@@ -1,19 +1,24 @@
 package se.kth.livetech.presentation.layout;
 
-import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
 
 import javax.swing.JPanel;
 
 import se.kth.livetech.contest.graphics.ContentProvider;
 import se.kth.livetech.presentation.graphics.Renderable;
+import se.kth.livetech.properties.IProperty;
+import se.kth.livetech.properties.ui.PanAndZoom;
 
 @SuppressWarnings("serial")
 public class LogoPresentation extends JPanel {
 	enum Logo { icpc, kth }
 	Renderable logoRenderer;
-	public LogoPresentation(Logo logo) {
+	IProperty panAndZoom;
+	public LogoPresentation(Logo logo, IProperty base) {
+		panAndZoom = base.get("logopz");
 		switch (logo) {
 		default:
 		case icpc:
@@ -24,10 +29,12 @@ public class LogoPresentation extends JPanel {
 			break;
 		}
 	}
+	
 	public void paintComponent(Graphics gr) {
-		Graphics2D g = (Graphics2D) gr;
-		logoRenderer.render(g, this.getSize());
-		g.setColor(Color.RED);
-		g.drawRect(10, 10, (int) this.getSize().getWidth() - 10, (int) this.getSize().getHeight() - 10);
+		Graphics2D g2d = (Graphics2D) gr;
+		Rectangle2D rect = PanAndZoom.getRect(panAndZoom, new Dimension(100, 100), this.getSize());
+		g2d.translate(rect.getX(), rect.getY());
+		logoRenderer.render(g2d, new Dimension((int)(rect.getWidth()), (int)(rect.getHeight())));
+		g2d.translate(-rect.getX(), -rect.getY());
 	}
 }

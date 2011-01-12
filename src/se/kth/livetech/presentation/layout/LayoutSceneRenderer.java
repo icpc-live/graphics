3e9716afaffb4921cf382313199d9b0a3bff2821
Mattias de Zalenski <zalenski@kth.se>
2011-01-12 20:57:49 +0100

    Added layout scene renderer.

---
+++ src/se/kth/livetech/presentation/layout/LayoutSceneRenderer.java
@@ -0,0 +1,62 @@
+package se.kth.livetech.presentation.layout;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+
+import se.kth.livetech.contest.graphics.ICPCColors;
+import se.kth.livetech.contest.graphics.ICPCImages;
+import se.kth.livetech.contest.graphics.RowFrameRenderer;
+import se.kth.livetech.presentation.contest.ContestStyle;
+import se.kth.livetech.presentation.graphics.ColoredTextBox;
+import se.kth.livetech.presentation.graphics.ImageRenderer;
+import se.kth.livetech.presentation.graphics.ImageResource;
+import se.kth.livetech.presentation.graphics.Renderable;
+
+public class LayoutSceneRenderer implements Renderable {
+	LayoutSceneUpdate scene;
+	
+	public void updateScene(LayoutSceneUpdate update) {
+		this.scene = update;
+	}
+	
+	@Override
+	public void render(Graphics2D g, Dimension d) {
+		render(g, this.scene);
+	}
+
+	private void render(Graphics2D g, LayoutSceneUpdate scene) {
+		AffineTransform at = g.getTransform();
+		g.translate(scene.getBounds().getX(), scene.getBounds().getY());
+		Content content = scene.getContent();
+		if (content != null) {
+			Renderable r;
+			if (content.isText()) {
+				ContestStyle style = (ContestStyle) content.getStyle();
+				r = new ColoredTextBox(content.getText(), ContestStyle.textBoxStyle(style));
+			} else if (content.isImage()) {
+				String imageName = content.getImageName();
+				ImageResource image = ICPCImages.getResource(imageName);
+				r = new ImageRenderer(imageName, image);
+			} else {
+				ContestStyle style = (ContestStyle) content.getStyle();
+				Color row1 = ICPCColors.BG_COLOR_1;
+				Color row2 = ICPCColors.BG_COLOR_2;
+				if (style == ContestStyle.rowBackground1) {
+					r = new RowFrameRenderer(row1, row2);
+				} else {
+					r = new RowFrameRenderer(row2, row1);
+				}
+			}
+			Dimension d = new Dimension();
+			d.setSize(scene.getBounds().getWidth() + .99, scene.getBounds().getHeight() + .99);
+			r.render(g, d);
+		}
+		
+		for (LayoutSceneUpdate sub : scene.getSubs()) {
+			render(g, sub);
+		}
+		g.setTransform(at);
+	}
+}

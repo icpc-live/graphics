cb4745d1a1ab96f932fa9e58b1d8b1cfae25baff
Mattias de Zalenski <zalenski@kth.se>
2011-01-12 19:57:16 +0100

    Added extended margin idea.

--- src/se/kth/livetech/presentation/layout/LayoutComponent.java
+++ src/se/kth/livetech/presentation/layout/LayoutComponent.java
@@ -8,4 +8,16 @@ public interface LayoutComponent {
 	public double getMargin();
 	public boolean isContent();
 	public Content getContent();
+	
+	/*
+	 * For more specific margins, something like:
+	interface Margin {
+		public double getTop();
+		public double getBottom();
+		public double getLeft();
+		public double getRight();
+		public double getAspectMin();
+		public double getAspectMax();
+	}
+	 */
 }

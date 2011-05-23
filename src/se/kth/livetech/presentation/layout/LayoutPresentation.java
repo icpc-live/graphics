4132c22213eef907c387f768b9153175efda7ff8
Aveen Ismail <avin@kth.se>
2011-05-23 19:05:44 +0200

    A changed score will cause the team-row to glow for 5 seconds. Bugs: (1) The whole row glows, not the individual problems. (2) The glowing does not follow the team-row in its animation, only the destination row glows.

--- src/se/kth/livetech/presentation/layout/LayoutPresentation.java
+++ src/se/kth/livetech/presentation/layout/LayoutPresentation.java
@@ -55,6 +55,7 @@ public class LayoutPresentation extends JPanel implements ContestUpdateListener 
     public int highlightedRow;
     public int highlightedProblem;
     private Map<Integer, Color> rowColorations;
+    private Map<Integer, Long> glowTimer;
 
 	ContestContent content;
     Contest contest;
@@ -130,7 +131,7 @@ public class LayoutPresentation extends JPanel implements ContestUpdateListener 
         colorProperties.addPropertyListener(colorListener);
 
         this.rowColorations = new TreeMap<Integer, Color>();
-
+        this.glowTimer = new HashMap<Integer, Long>();
 
 
 
@@ -313,13 +314,13 @@ public class LayoutPresentation extends JPanel implements ContestUpdateListener 
 		}
 		*/
 
-        Contest c = this.contest;
 		ISceneDescriptionUpdater backgroundUpdater;
 		backgroundUpdater = updater.getSubLayoutUpdater(-1);
 		backgroundUpdater.setDirection(ISceneDescription.Direction.VERTICAL);
         boolean glow;
 		for (int i = 1; i <= 17; ++i) {
-            glow = false;
+            glow = shouldGlow(i);
+            /*
             Team team = c.getRankedTeam(i);
             int id = team.getId();
             TeamScore ts = c.getTeamScore(id);
@@ -327,6 +328,7 @@ public class LayoutPresentation extends JPanel implements ContestUpdateListener 
             if (ts.getSolved() != prev.getSolved()) {
                 glow = true;
             }
+            */
 			ContestComponents.teamBackground(this.content, i, backgroundUpdater, glow);
 
 		}
@@ -336,6 +338,26 @@ public class LayoutPresentation extends JPanel implements ContestUpdateListener 
 		
 		return updater;
 	}
+    
+    private boolean shouldGlow(int teamRank){
+        boolean glow = false;
+        Team team = this.contest.getRankedTeam(teamRank);
+        int teamID = team.getId();
+        long now = System.currentTimeMillis();
+
+        TeamScore ts = this.contest.getTeamScore(teamID);
+        TeamScore prev = recent.get(teamID);
+        if (ts.getSolved() != prev.getSolved()) {
+            if(glowTimer.containsKey(teamID)) {
+                glow = (now - glowTimer.get(teamID)) < 5000;
+            } else {
+                glow = true;
+                glowTimer.put(teamID, new Long(now));
+            }
+        }
+
+        return glow;
+    }
 
 	public void paintScene(Graphics2D g, Rectangle2D row, SceneDescription updater) {
 		

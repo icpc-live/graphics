f158867679f63e44b83b30bd52906a22fa2e09ea
Mattias de Zalenski <zalenski@kth.se>
2011-05-29 08:13:07 -0400

    Setup noname control panel property on all noname startups.

--- src/se/kth/livetech/communication/LiveClient.java
+++ src/se/kth/livetech/communication/LiveClient.java
@@ -242,6 +242,8 @@ public class LiveClient {
 			}
 			if (opts.isLayout()) {
 				localState.getHierarchy().getProperty("live.clients.noname").set("mode", "layout");
+			}
+			if (name.equals("noname")) {
 				localState.getHierarchy().getProperty("live.control.noname.panel1").set("name", "noname");
 			}
 			if (opts.isLive() || opts.isLayout()) {

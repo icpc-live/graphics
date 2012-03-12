b8dc876af48c6666d56901888cf71ff1a6d11e06
Mattias de Zalenski <zalenski@cantab.net>
2012-03-12 01:47:32 +0100

    Added problem symbol, color-name and color-rgb to model.

--- src/se/kth/livetech/contest/model/Problem.java
+++ src/se/kth/livetech/contest/model/Problem.java
@@ -2,4 +2,7 @@ package se.kth.livetech.contest.model;
 
 /** Problem entity. */
 public interface Problem extends Entity {
+	public String getSymbol();
+	public String getColorName();
+	public String getColorRgb();
 }
--- src/se/kth/livetech/contest/model/impl/ProblemImpl.java
+++ src/se/kth/livetech/contest/model/impl/ProblemImpl.java
@@ -5,11 +5,33 @@ import java.util.Map;
 import se.kth.livetech.contest.model.Problem;
 
 public class ProblemImpl extends EntityImpl implements Problem {
+	private String symbol;
+	private String colorName;
+	private String colorRgb;
+
 	public ProblemImpl(Map<String, String> attrs) {
 		super(attrs);
+		symbol = attrs.get("symbol");
+		colorName = attrs.get("color-name");
+		colorRgb = attrs.get("color-rgb");
 	}
 
 	public String getType() {
 		return "problem";
+	}
+
+	@Override
+	public String getSymbol() {
+		return symbol;
+	}
+
+	@Override
+	public String getColorName() {
+		return colorName;
+	}
+
+	@Override
+	public String getColorRgb() {
+		return colorRgb;
 	}
 }
